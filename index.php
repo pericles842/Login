73a1f171436dfdbc879929e991309e92d1fa5a8f
pericles842 <slouis482@gmail.com>
2022-06-18 18:35:22 -0400

    18-6-2022

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a mi App </title>
+    <!-- Styles-->
+    <link rel="stylesheet" href="assets/css/index.css">
+    <!-- Fonts-->
+    <link rel="preconet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Ubuntu:wght@300&display=swap" >
+
+</head>
+<body>
+    <div class="box">
+        <div class="words">
+            <h1>Bienvenido a mi App</h1>
+            <p>Por favor ingresa o regístrate</p>
+            <div class="box-flex">
+                <a href="login.php">Iniciar Sesion</a>
+                <p class="o">
+                    or
+                </p>
+                <a href="sigup.php">regístrate</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
