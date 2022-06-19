5092c569300b2985c7e7fe65dc43981a983726e1
pericles842 <slouis482@gmail.com>
2022-06-19 19:09:59 -0400

    master

--- singup.php
+++ singup.php
@@ -6,20 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrate</title>
      <!-- Styles-->
-     <link rel="stylesheet" href="assets/css/singup.css">
+    <link rel="stylesheet" href="assets/css/singup.css">
     <!-- Fonts-->
     <link rel="preconet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Ubuntu:wght@300&display=swap" >
 </head>
 <body>
     <div class="box-flex">
-        <div class="">
+        <div class="words">
             <h1>Crear Cuenta</h1>
             <p>Crea una cuenta para guardar todos <br>
             tus datos en la nube, e iniciar la experiencia App </p>
         </div>
-        <div>
+        <div class="box-inputs-loguin">
             <form>
-                <div>
+                <div class="inputs-loguin">
                     <p>Correo</p>
                     <input type="email" >
                     <p>Usuario</p>
@@ -27,7 +27,7 @@
                     <p>Contraseña</p>
                     <input type="password">
                     <input type="submit" value="Enviar">
-                    </div>
+                </div>
             </form>
         </div>
     </div>
